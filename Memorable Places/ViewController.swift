32655ac025bffd63b1b011aa2a1558a52e480e44
Reid Kostenuk <reidkostenuk@gmail.com>
2016-07-27 14:28:54 -0600

    Initial Commit

---
+++ Memorable Places/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Memorable Places
+//
+//  Created by Reid Kostenuk on 2016-07-27.
+//  Copyright © 2016 App Monkey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
